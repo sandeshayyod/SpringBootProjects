cc0de5fbd7c146ae2a58c3d15082aef7b819b853
sandeshayyod-git <sandesh.ayyod89@gmail.com>
2021-02-22 23:58:19 +0530

    COVIDTRACKER - new addons WIP

--- coronavirus-tracker/src/main/java/com/homeproject/coronavirustracker/CoronavirusTrackerApplication.java
+++ coronavirus-tracker/src/main/java/com/homeproject/coronavirustracker/CoronavirusTrackerApplication.java
@@ -2,12 +2,14 @@ package com.homeproject.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronavirusTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoronavirusTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoronavirusTrackerApplication.class, args);
+    }
 
 }
